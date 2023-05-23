8f45f5bee935bee3cd76a4b1f69df0a21b2b95c7
Wesley Smith <wesy32@gmail.com>
2023-05-23 09:55:52 -0700

    Syscall notes

---
+++ wk9/filesize.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[]) {
+    // FILE* fp = fopen(argv[1], "r");
+
+    // fseek(fp, 0, SEEK_END);
+
+    // long endbytes = ftell(fp);
+
+    // printf("Size: %ld\n", endbytes);
+    // fclose(fp);
+
+    // struct stat statbuf;
+    // stat(argv[1], &statbuf);
+    // printf("Size=%ld\n", statbuf.st_size);
+
+    int fd = open(argv[1]);
+    
+}
